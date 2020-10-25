575fe3242f9c3f3592f74f6c5b0eeb1d91d63daf
Han Dai <github@daihan.me>
2020-10-25 00:16:00 +0000

    Chanllenge_01 with Detour

---
+++ Challenge_01/patch_with_InsertCodePatch.py
@@ -0,0 +1,25 @@
+from patcherex.backends.detourbackend import DetourBackend
+from patcherex.patches import *
+
+backend = DetourBackend("./BrakeFlasher_AVR_Vuln.ino.elf")
+
+patches = []
+
+code = '''
+mov r22, r17
+ldi r23, 0x00
+ldi r24, 0x00
+ldi r25, 0x00
+ldi r20, 0x0a
+jmp 0xfb8
+'''
+
+# Line 58: Serial.print(speed_value/256);
+patches.append(InsertCodePatch(0xf84, code))
+
+# Line 65: if (speed_value > 0 && previous_brake_state != brake_state){
+patches.append(InlinePatch(0xff4, "or r16, r17\nnop\n"))
+patches.append(InlinePatch(0xff8, "breq +0x16\n")) # 0xff8 + 0x16 = 0x100e
+
+backend.apply_patches(patches)
+backend.save("/tmp/Challenge01_Patched")

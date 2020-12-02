5f338fb2e8eedb8b87335f7e5dbacc768a0a4f18
Antonio Bianchi <antoniob@cs.ucsb.edu>
2020-12-02 02:45:50 -0500

    public

---
+++ Challenge_02/patch.py
@@ -0,0 +1,42 @@
+from argparse import ArgumentParser
+from patcherex.backends.detourbackend import DetourBackend
+from patcherex.patches import *
+
+parser = ArgumentParser()
+parser.add_argument("original")
+parser.add_argument("patched")
+args = parser.parse_args()
+
+backend = DetourBackend(args.original, variant="stm32")
+patches = []
+
+typedef = '''
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+'''
+transmit_code = '''
+void rx_brake_routine( unsigned char buff[], void *bumper ){
+	uint16_t speed_value;  
+	uint8_t brake_switch;
+	speed_value  = (buff[3] << 8) + buff[2];
+	brake_switch = (buff[4] & 0b00001100) >> 2;
+	((unsigned char*)bumper)[13] = (brake_switch) ? 1 : 0;
+
+	if ( ((unsigned char*)bumper)[13] ) {
+		if ((speed_value > 0) && ( ((unsigned char*)bumper)[12] != ((unsigned char*)bumper)[13] )){ 
+			((unsigned char*)bumper)[14] = 1;
+			((unsigned int*)bumper)[4] = ((unsigned char*)bumper)[12];
+		}
+	}
+	else {
+	    ((unsigned char*)bumper)[14] = 0;
+	}
+	((unsigned char*)bumper)[12] = ((unsigned char*)bumper)[13];
+}
+'''
+
+transmit_code = typedef + transmit_code.replace("\n", " ")
+
+patches.append(ReplaceFunctionPatch(0x80003AC, 0x38, transmit_code))
+backend.apply_patches(patches)
+backend.save(args.patched)
---
+++ README.md
@@ -0,0 +1,34 @@
+# AMP patches
+
+The code has been tested on Ubuntu 18.04.
+
+## Installation
+
+```bash
+# install and upgrade pip
+sudo apt install python3-pip
+pip3 install --upgrade pip
+
+# install dependencies
+sudo apt install nasm clang clang-10 gcc-avr binutils-avr avr-libc
+pip3 install git+https://github.com/angr/angr-platforms.git@wip/avr
+
+# install patcherex
+pip3 install git+https://github.com/angr/patcherex.git@feat/multiarch
+```
+
+## Usage
+
+### Challenge_01
+```bash
+cd Challenge_01
+./patch.py /path/to/original_binary /tmp/patched_binary
+```
+`/path/to/original_binary` should be the `BrakeFlasher_AVR_Vuln.ino.elf` file (SHA256: `6D23C15E2EA583A45DCCE52CDDA155E9D3838D4E4DC4C7588D200D5762FEAD61`), from the `AMP_Challenge-Problems` repository.
+
+### Challenge_02
+```bash
+cd Challenge_02
+./patch.py /path/to/original_binary /tmp/patched_binary
+```
+`/path/to/original_binary` should be the `Nucleo-32.elf` file (SHA256: `1B3ABBF957197A7557E59DE2AEA90EA334303086A38431D2CE94CBA7C9A35205`), from the `AMP_Challenge-Problems` repository.
